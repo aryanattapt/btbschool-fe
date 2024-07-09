b4e3688e206280ee88e0333f517f088acbdedc75
Aryanatta P.T <aryanattapt28@outlook.co.id>
2024-07-09 22:06:12 +0700

    trial flowbite react

---
+++ datamodelling.js
@@ -0,0 +1,120 @@
+const homePage = {
+    "language": ["EN", "ID"],
+    "instagramapi": {
+        "url": "",
+        "requestpayload": {}
+    },
+    "headersection": {
+        "logo": {
+            "EN":  {
+                "url": ""
+            },
+            "ID": {
+                "url": ""
+            }
+        },
+        "navbarmenu": {
+            "ID": [
+                {
+                    "name": "Daftar",
+                    "isactive": true,
+                    "url": "/daftar",
+                    "menus": []
+                }, 
+                {
+                    "name": "Tentang Kami",
+                    "isactive": true,
+                    "url": "#",
+                    "menus": [
+                        {
+                            "name": "Profil",
+                            "isactive": true,
+                            "url": "/profil"
+                        }
+                    ]
+                }
+            ]
+        },
+        "carousel": {
+            "ID": [
+                {
+                    "contenturl": "",
+                    "contenttype": "",
+                    "text": ""
+                }
+            ]
+        }
+    },
+    "gradesection": {
+        "EN": {
+            "title": "",
+            "desc": "",
+            "gradeList": [
+                {
+                    "contenturl": "",
+                    "title": "",
+                    "desc": "",
+                    "url": "",
+                    "isactive": true
+                }
+            ]
+        },
+    },
+    "floatingmenusection": [
+        {
+            "logourl": "",
+            "colorscheme": "", 
+            "name": "Daftar",
+            "isactive": true,
+            "url": "/daftar",
+            "menus": []
+        },
+    ],
+    "footersection": {
+        "footermenu": {
+            "ID": [
+                {
+                    "name": "Daftar",
+                    "isactive": true,
+                    "url": "/daftar",
+                    "menus": []
+                }, 
+                {
+                    "name": "Tentang Kami",
+                    "isactive": true,
+                    "url": "#",
+                    "menus": [
+                        {
+                            "name": "Profil",
+                            "isactive": true,
+                            "url": "/profil"
+                        }
+                    ]
+                }
+            ]
+        },
+        "socialmedia": [
+            {
+                "name": "Youtube",
+                "logourl": "",
+                "url": "",
+                "isactive": true
+            }
+        ],
+        "addresses": [
+            {
+                "buildingname": "",
+                "street": "",
+                "kelurahan": "",
+                "kecamatan": "",
+                "kotamadya": "",
+                "provinsi": "",
+                "kodepos": "",
+                "isactive": true,
+                "googlemapurl": "",
+                "phonenumber": "",
+                "faxnumber": ""
+            }
+        ]
+    }
+}
---
+++ src/app/admin/page.js
@@ -0,0 +1,11 @@
+import NavBar from "./components/navbar";
+import SideBar from "./components/sidebar";
+
+const AdminPage = () => {
+    return <>
+        <NavBar/>
+        <SideBar/>
+    </>;
+}
+
+export default AdminPage;
