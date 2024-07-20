a69ed3f1e8c913f4e53bf7b7e3a7f3238c0cf7ce
Aryanatta P.T <aryanattapt28@outlook.co.id>
2024-07-21 03:39:25 +0700

    update strokewidth

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -121,7 +121,7 @@ const HomePage = () => {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    stroke-width="2"
+                    strokeWidth="2"
                     d="M15 19l-7-7 7-7"
                   />
                 </svg>
