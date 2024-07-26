42aea9035e86ead63f8a8e7c391a851de71cd898
dinaputrikh <dinaputrikh@gmail.com>
2024-07-26 23:38:18 +0700

    responsive contact and maps

--- src/app/(clientpage)/contact/_layouts/pagging.jsx
+++ src/app/(clientpage)/contact/_layouts/pagging.jsx
@@ -3,28 +3,28 @@ const PaggingLayouts = () => {
   return (
     <>
       <header className="flex flex-row items-start justify-between text-[#00305E] border-b">
-        <div className="inline-flex gap-12">
+        <div className="inline-flex md:gap-9 gap-12">
           <div className="border-r-2">
-            <div className="pr-10 pl-32">
-              <h1 className="text-[25px] text-center font-semibold py-10">
-                Kontak
+            <div className="pr-10 pl-10 md:pr-40 md:pl-20">
+              <h1 className="md:text-[35px] text-center font-semibold py-10 mx-[6px] md:mx-[15px]">
+                KONTAK
               </h1>
             </div>
           </div>
-          <div className="inline-flex gap-20 text-[20px] text-center text-gray-500 border-gray-200 dark:text-gray-400 dark:border-gray-700">
-            <ul className="flex flex-wrap -mb-px py-10">
-              <li className="px-10">
+          <div className="inline-flex gap-20 md:text-[25px] text-[14px] text-center text-gray-500 border-gray-200 dark:text-gray-400 dark:border-gray-700">
+            <ul className="flex flex-wrap py-10 md:py-10">
+              <li className="px-[1px] md:px-[100px]">
                 <a
-                  href="/contact#contact"
+                  href="/contact#hubungi-kami"
                   className="inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600"
                   aria-current="page"
                 >
                   Hubungi Kami
                 </a>
               </li>
-              <li className="px-10">
+              <li className="px-[10px]">
                 <a
-                  href="/contact#location"
+                  href="/contact#lokasi-kami"
                   className="inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600"
                 >
                   Lokasi Kami
--- src/app/(clientpage)/contact/page.jsx
+++ src/app/(clientpage)/contact/page.jsx
@@ -1,36 +1,133 @@
 import ContactForm from "./_layouts/form";
 import Banner from "./_layouts/banner";
 import Pagging from "./_layouts/pagging";
-import { HiPhone } from "react-icons/hi";
+import { HiPhone, HiMail } from "react-icons/hi";
+import { AiOutlineWhatsApp } from "react-icons/ai";
 
 const ContactPage = () => {
   return (
     <>
       <Banner />
       <Pagging />
-      <h1 className="mt-10 md:mx-[76px] text-[#00305E] md:text-[25px]">
-        HUBUNGI KAMI
-      </h1>
-      <div className="md:mx-[76px] text-[#00305E] flex flex-nowrap mb-10">
-        <div className="md:mr-[70px]">
-          <p className="md:text-[45px] my-5 font-bold">
-            Ambil langkah awal untuk bergabung bersama kami!
-          </p>
-          <div className="flex-col">
-            <ContactForm />
+      <div id="hubungi-kami md:mx-[76px] mx-[10px] ">
+        <h1 className="mt-10 text-[#00305E] md:text-[50px] text-[25px] font-semibold	">
+          HUBUNGI KAMI
+        </h1>
+        <div className="md:mx-[76px] text-[#00305E] grid grid-cols-1 md:grid-cols-2 mb-10">
+          <div className="md:mr-[70px] mr-[50px]">
+            <p className="md:text-[45px] text-[16px] my-5 text-black">
+              Ambil langkah awal untuk bergabung bersama kami!
+            </p>
+            <div className="flex-col">
+              <ContactForm />
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="md:w-[372px] md:h-[629px] bg-[#EF802B] rounded-3xl">
-            <div className="text-white">
-              <div className="header px-10">
-                <h1 className="font-bold text-[35px] pt-10">BTB SCHOOL PLUIT TIMUR</h1>
-                <div className="address mt-2 leading-loose text-[20px]">
-                  Jl.   Pluit Timur Blok MM
+          <div>
+            <div className="md:w-[372px] md:h-auto w-[372px] h-auto bg-[#EF802B] rounded-3xl mx-5 mt-8 place-content-center">
+              <div className="text-white pb-10">
+                <div className="header px-5 md:px-15 ">
+                  <h1 className="font-bold md:text-[21px] text-[15px] pt-10">
+                    BTB SCHOOL PLUIT TIMUR
+                  </h1>
+                  <div className="address mt-2 leading-loose md:text-[20px] text-[13px]">
+                    Jl. Pluit Timur Blok MM
+                  </div>
+                  <div className="flex flex-nowrap">
+                    <div>
+                      <HiPhone className="md:size-6" />
+                    </div>
+                    <div className="ml-4">
+                      <p className="md:text-[20px] text-[13px]">
+                        (021)669 8888
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-nowrap md:mt-2">
+                    <div>
+                      <AiOutlineWhatsApp className="md:size-6" />
+                    </div>
+                    <div className="ml-4">
+                      <p className="md:text-[20px] text-[13px]">085695333888</p>
+                    </div>
+                  </div>
                 </div>
-                <div><HiPhone/></div>
+                <div className="header px-5 md:px-15">
+                  <h1 className="font-bold md:text-[21px] pt-10">
+                    BTB School Pluit Putri
+                  </h1>
+                  <div className="address mt-2 leading-loose md:text-[20px] text-[13px]">
+                    Jl.Pluit Timur Blok MM
+                  </div>
+                  <div className="flex flex-nowrap">
+                    <div>
+                      <HiPhone className="md:size-6" />
+                    </div>
+                    <div className="ml-4">
+                      <p className="md:text-[20px] text-[13px]">
+                        (021)669 8888
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-nowrap md:mt-2">
+                    <div>
+                      <AiOutlineWhatsApp className="md:size-6" />
+                    </div>
+                    <div className="ml-4">
+                      <p className="md:text-[20px] text-[13px]">085695333888</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="py-3 flex items-center text-sm text-gray-800 after:flex-1 after:border-t after:border-gray-200 after:mx-6 dark:text-white dark:after:border-neutral-600"></div>
+
+                <div className="header px-5 md:px-15">
+                  <div className="flex flex-nowrap md:mt-2">
+                    <div>
+                      <HiMail className="md:size-6" />
+                    </div>
+                    <div className="ml-4">
+                      <p className="md:text-[20px] text-[13px]">
+                        info@btbschool.org
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-3 flex items-center text-sm text-gray-800 after:flex-1 after:border-t after:border-gray-200 after:mx-6 dark:text-white dark:after:border-neutral-600"></div>
+
+      <div id="lokasi-kami" className="md:m-[60px] m-[8px]">
+        <h1 className="text-[#00305E] md:text-[50px] text-[25px] font-semibold	">
+          LOKASI KAMI
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div>
+            <h3 className="md:text-[35px] text-[18px] text-black font-medium	">
+              BTB School Pluit Timur
+            </h3>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7934.134621362215!2d106.79109025869137!3d-6.12164295274717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1deb383b5aad%3A0x88c7848de94ced29!2sBTB%20School%20Pluit%20Timur!5e0!3m2!1sid!2sid!4v1722009912125!5m2!1sid!2sid"
+              className="w-[148px] h-[148px] md:w-[548px] md:h-[474px] "
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="ml-[1px] md:ml-[30px] mt-6 md:mt-0">
+            <h3 className="md:text-[35px] text-[18px] text-black font-medium	">
+              BTB School Pluit Timur
+            </h3>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.080070999209!2d106.78967467369849!3d-6.119924360009179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1d84049afab9%3A0x79d9dbfb470b13dd!2sBTB%20School!5e0!3m2!1sid!2sid!4v1722008582899!5m2!1sid!2sid"
+              className="w-[148px] h-[148px] md:w-[548px] md:h-[474px]"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
         </div>
       </div>
