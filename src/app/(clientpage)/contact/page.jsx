24c3e998051f0c45cfa1ad134bbe5b2701be7a85
dinaputrikh <dinaputrikh@gmail.com>
2024-08-10 19:56:51 +0700

    responsive contact and btb care

--- src/app/(clientpage)/contact/page.jsx
+++ src/app/(clientpage)/contact/page.jsx
@@ -22,7 +22,7 @@ const ContactPage = () => {
               <ContactForm />
             </div>
           </div>
-          <div>
+          <div className="md:mt-0 mt-5">
             <div className="md:h-screen h-auto bg-[#EF802B] rounded-3xl mx-5 py-4 md:py-0 place-content-center">
               <div className="text-white pb-10">
                 <div className="header px-5 md:px-15 ">
@@ -117,7 +117,7 @@ const ContactPage = () => {
             </h3>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7934.134621362215!2d106.79109025869137!3d-6.12164295274717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1deb383b5aad%3A0x88c7848de94ced29!2sBTB%20School%20Pluit%20Timur!5e0!3m2!1sid!2sid!4v1722009912125!5m2!1sid!2sid"
-              className="w-[148px] h-[148px] md:w-[548px] md:h-[474px] "
+              className="w-[248px] h-[248px] md:w-[548px] md:h-[474px] "
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
@@ -129,7 +129,7 @@ const ContactPage = () => {
             </h3>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.080070999209!2d106.78967467369849!3d-6.119924360009179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1d84049afab9%3A0x79d9dbfb470b13dd!2sBTB%20School!5e0!3m2!1sid!2sid!4v1722008582899!5m2!1sid!2sid"
-              className="w-[148px] h-[148px] md:w-[548px] md:h-[474px]"
+              className="w-[248px] h-[248px] md:w-[548px] md:h-[474px]"
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
