6b0a1549ffc55e1cc563087b6c81c4731dc31ed6
Aryanatta P.T <aryanattapt28@outlook.co.id>
2024-08-12 21:00:56 +0700

    up bulletin

--- src/app/(clientpage)/bulletinspotlight/page.jsx
+++ src/app/(clientpage)/bulletinspotlight/page.jsx
@@ -5,76 +5,74 @@ import PDFReaderFlipBook from '../../_components/pdfreaderflipbook';
 import PDFThumbnail from './_components/pdfpreview';
 import Banner from './_components/Banner';
 import Pagging from './_components/Pagging';
-import {BulletinSpotlightPayload} from '../../../../data';
-import {useLanguageStore} from '../../../../store/language.store';
 import { GetConfig } from '../../../../services/config.service';
 import Swal from 'sweetalert2';
 
-
-const FlipbookModal = ({ url, isOpen, setIsOpen }) => {
-    const modalStyle = useMemo(() => ({ height: "450px", width: "800px"}), []);
-    return (
-        <Modal show={isOpen} dismissible size="4xl" onClose={() => setIsOpen(false)}>
-            <Modal.Body>
-                <div style={modalStyle} className='overflow-hidden'>
-                    <PDFReaderFlipBook url={url} width={400} height={600}/>
-                </div>
-            </Modal.Body>
-        </Modal>
-    );
-};
-
 const BulletinSpotlightPage = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const openModal = useCallback(() => setIsModalOpen(true), []);
-    const closeModal = useCallback(() => setIsModalOpen(false), []);
-    const [bulletinSpotlightData, setBulletinSpotlightData] = useState(BulletinSpotlightPayload);
-    const { language } = useLanguageStore();
     const [payload, setPayload] = useState([])
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [pdfUrl, setPdfUrl] = useState(""); // Added state to store the PDF URL
+
+    const openModal = useCallback((url) => {
+        setPdfUrl(url); // Set the PDF URL when opening the modal
+        setIsModalOpen(true);
+    }, []);
+
+    const closeModal = useCallback(() => setIsModalOpen(false), []);
+
+    const FlipbookModal = ({ url, isOpen, setIsOpen }) => {
+        const modalStyle = useMemo(() => ({ height: "570px", width: "800px" }), []);
+        return (
+            <Modal show={isOpen} dismissible size="4xl" onClose={() => setIsOpen(false)}>
+                <Modal.Body>
+                    <div style={modalStyle} className='overflow-hidden'>
+                        <PDFReaderFlipBook url={url} width={400} height={700} />
+                    </div>
+                </Modal.Body>
+            </Modal>
+        );
+    };
 
     useEffect(() => {
         GetConfig('bulletinspotlight', {}).then(res => {
-          console.log(res);
-          setPayload(res);
+            console.log(res);
+            setPayload(res);
         }).catch((err) => {
-          Swal.fire({
-            allowOutsideClick: false,
-            title: 'Error Notification!',
-            text: err,
-            icon: 'error',
-          });
+            Swal.fire({
+                allowOutsideClick: false,
+                title: 'Error Notification!',
+                text: err,
+                icon: 'error',
+            });
         })
-      }, [])
+    }, [])
 
     return (
         <>
-        <Banner />
-        <Pagging />
-        <div className="mt-10 mb-5 pl-32 text-[#00305E]">
-            <h1 className="text-[35px] font-bold">
-                {bulletinSpotlightData[language].title}
-            </h1>
-            <div className="text-[30px]">
-                {bulletinSpotlightData[language].desc}
-            </div>
-            <div className="flex flex-col items-center gap-2">
+            <Banner />
+            <Pagging />
+            <div className="mt-10 mb-5 pl-32 text-[#00305E]">
+                <FlipbookModal
+                    url={pdfUrl} // Pass the URL to FlipbookModal
+                    isOpen={isModalOpen}
+                    setIsOpen={closeModal}
+                />
                 {
-                    payload.map((val, idx) => (
-                        <div key={idx}>
-                            <FlipbookModal 
-                                isOpen={isModalOpen} 
-                                setIsOpen={closeModal} 
-                                url={val.attachment[0].fileURL}
-                            />
-                            <PDFThumbnail 
-                                file={val.attachment[0].fileURL} 
-                                onClick={openModal} 
-                            />
+                    payload.map((val, idx) => {
+                        return <div key={idx}>
+                            <div className="text-[30px]">
+                                {val.bulletintitle}
+                            </div>
+                            <div className="flex flex-col items-center gap-2">
+                                <PDFThumbnail
+                                    file={val.attachment[0].fileURL}
+                                    onClick={() => openModal(val.attachment[0].fileURL)} // Pass the URL to openModal
+                                />
+                            </div>
                         </div>
-                    ))
+                    })
                 }
             </div>
-        </div>
         </>
     );
 };
