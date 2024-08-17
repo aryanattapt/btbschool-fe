adda9b5c621d49331ae8fa7d1fa04f7b9be540fa
Aryanatta P.T <aryanattapt28@outlook.co.id>
2024-08-17 16:40:24 +0700

    excel online regis field

--- src/utils/admin/registration/export.js
+++ src/utils/admin/registration/export.js
@@ -15,21 +15,21 @@ const siblingExport = (data) => {
 export const admRegisManagerExportObjectBuilder = (datas = []) => {
 	if (datas?.length > 0) {
 		const result = datas.map((data, index) => ({
-			No: index + 1,
+			"No": index + 1,
 			"Registration Code": data?.registrationcode,
 			"Registration Date": data?.registereddate,
-			"Student ID": data?._id,
-			NISN: data?.nisn,
+			"Student ID": "",
+			"NISN": "",
 			"Student Name": `${data?.firstname} ${data?.middlename} ${data?.lastname}`,
 			"Place & Date of Birth": `${data?.birthplace}, ${dateDetailDisplay(
 				data?.birthdate
 			)}`,
-			Nationality: data?.nationality,
-			Religion: data?.religion,
-			Gender: data?.gender,
-			Address: data?.address,
-			Telepon: data?.phoneno,
-			Email: data?.email,
+			"Nationality": data?.nationality,
+			"Religion": data?.religion,
+			"Gender": data?.gender,
+			"Address": data?.address,
+			"Telepon": data?.phoneno,
+			"Email": data?.email,
 			"Blood Group": data?.bloodgroup,
 			"Doctor's Name": data?.doctorname,
 			"Doctor's Telephone": data?.doctorphone,
@@ -40,9 +40,9 @@ export const admRegisManagerExportObjectBuilder = (datas = []) => {
 			"Please Explain the Nature of the medication, frequency of usage and how it administered":
 				data?.naturemedication,
 			"Does your child have an allergy to any following?":
-				data?.natureofallergy,
+				data?.alergicoption,
 			"Please specify the nature of the allergy (doctor's certificate required)":
-				"-",
+				data?.natureofallergy,
 			"Does your child suffer any limitation on physical activity?":
 				data?.limitationofphysical,
 			"Has your child had any surgery or operation?": data?.surgeryoperation,
@@ -57,26 +57,26 @@ export const admRegisManagerExportObjectBuilder = (datas = []) => {
 			"Father Place & Date of Birth": `${
 				data?.fatherbirthplace
 			}, ${dateDetailDisplay(data?.fatherbirthdate)}`,
-			Mobile: data?.fatherphoneno,
-			Email: data?.fatheremail,
-			"Marital Status": data?.fathermaritalstatus,
-			Occupation: data?.fatheroccupation,
-			"Conpany Name": data?.fathercompanyname,
-			"Bussiness Address": data?.fatherbusinessAddress,
-			Telp: data?.fathertelephone,
-			Fax: data?.fatherfax,
+			"Father's Mobile": data?.fatherphoneno,
+			"Father's Email": data?.fatheremail,
+			"Father's Marital Status": data?.fathermaritalstatus,
+			"Father's Occupation": data?.fatheroccupation,
+			"Father's Conpany Name": data?.fathercompanyname,
+			"Father's Bussiness Address": data?.fatherbusinessAddress,
+			"Father's Telp": data?.fathertelephone,
+			"Father's Fax": data?.fatherfax,
 			"Mother's Name": data?.mothername,
 			"Mother Place & Date of Birth": `${
 				data?.motherbirthplace
 			}, ${dateDetailDisplay(data?.motherbirthdate)}`,
-			Mobile: data?.motherphoneno,
-			Email: data?.motheremail,
-			"Marital Status": data?.mothermaritalstatus,
-			Occupation: data?.motheroccupation,
-			"Conpany Name": data?.mothercompanyname,
-			"Bussiness Address": data?.motherbusinessAddress,
-			Telp: data?.mothertelephone,
-			Fax: data?.motherfax,
+			"Mother's Mobile": data?.motherphoneno,
+			"Mother's Email": data?.motheremail,
+			"Mother's Marital Status": data?.mothermaritalstatus,
+			"Mother's Occupation": data?.motheroccupation,
+			"Mother's Conpany Name": data?.mothercompanyname,
+			"Mother's Bussiness Address": data?.motherbusinessAddress,
+			"Mother's Telp": data?.mothertelephone,
+			"Mother's Fax": data?.motherfax,
 			"Emergency Name": data?.emergencycontactname,
 			"Emergency Relationship to Student": data?.emergencycontactrelaction,
 			"Emergency Telp": data?.emergencycontactphoneno,
@@ -85,9 +85,9 @@ export const admRegisManagerExportObjectBuilder = (datas = []) => {
 			"Recommended by someone ?": data?.recommendedoption,
 			"Name of BTB Parent": data?.btbparentnamerec,
 			"Name of BTB Student": data?.btbstudentnamerec,
-			Grade: data?.btbstudentgraderec,
+			"Grade": data?.btbstudentgraderec,
 			"Phone No.": data?.btbstudentphonehomerec,
-			Mobile: data?.btbstudentphonemobilerec,
+			"Mobile": data?.btbstudentphonemobilerec,
 		}));
 		return result;
 	} else {
