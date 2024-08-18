0efc7e5915d780b341d5c20a8618ea027ffa0719
Aryanatta P.T <aryanattapt28@outlook.co.id>
2024-08-19 05:37:12 +0700

    fix floating button text

--- src/app/_components/floatingbutton.jsx
+++ src/app/_components/floatingbutton.jsx
@@ -39,21 +39,21 @@ const FloatingButtons = () => {
         <button className="w-[54.21px] h-[54.21px] bg-[#EF802B] flex justify-center items-center transform rotate-45 mb-4 transition-transform duration-300 group-hover:scale-110">
           <FaCalendar className="text-white text-xl -rotate-45" />
         </button>
-        <span className="absolute right-full ml-2 text-white bg-black px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Calendar</span>
+        <span className="absolute right-full mr-2 text-white bg-black px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Calendar</span>
       </div>
 
       <div className="relative group flex items-center">
         <button className="w-[54.21px] h-[54.21px] bg-[#EF802B] flex justify-center items-center transform rotate-45 mb-4 transition-transform duration-300 group-hover:scale-110">
           <FaQuestion className="text-white text-xl -rotate-45" />
         </button>
-        <span className="absolute left-full ml-2 text-white bg-black px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Help</span>
+        <span className="absolute right-full mr-2 text-white bg-black px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Help</span>
       </div>
 
       <div className="relative group flex items-center">
         <button className="w-[54.21px] h-[54.21px] bg-[#EF802B] flex justify-center items-center transform rotate-45 mb-4 transition-transform duration-300 group-hover:scale-110">
           <FaPhoneAlt className="text-white text-xl -rotate-45" />
         </button>
-        <span className="absolute left-full ml-2 text-white bg-black px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Call</span>
+        <span className="absolute right-full mr-2 text-white bg-black px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">Call</span>
       </div>
     </div>
   );
