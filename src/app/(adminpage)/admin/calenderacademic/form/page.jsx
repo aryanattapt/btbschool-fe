15375073666b9b242b525e12532f3b9df6948ec6
Aryanatta P.T <aryanattapt28@outlook.co.id>
2024-08-20 21:30:36 +0700

    fix error build

--- src/app/(adminpage)/admin/calenderacademic/form/page.jsx
+++ src/app/(adminpage)/admin/calenderacademic/form/page.jsx
@@ -1,47 +1,45 @@
-'use client'
-import {
-    GetConfig,
-} from '../../../../../../services/config.service';
+'use client';
+
+import { GetConfig } from '../../../../../../services/config.service';
 import { Suspense, useEffect, useState } from 'react';
-import CalenderAcademicMainForm from './_components/main.form'
+import CalenderAcademicMainForm from './_components/main.form';
 import { useSearchParams } from 'next/navigation';
 import Swal from 'sweetalert2';
-import {
-    detransformJsonLanguage,
-} from '../../../../../../helpers/jsontransform.helper'
+import { detransformJsonLanguage } from '../../../../../../helpers/jsontransform.helper';
 
-const CalenderAcademicFormPage = () => {
-    const [payload, setPayload] = useState({})
-    const searchParams = useSearchParams()
-    const id = searchParams.get("id")
+const _CalenderAcademicFormPage = () => {
+    const [payload, setPayload] = useState({});
+    const searchParams = useSearchParams();
+    const id = searchParams.get("id");
 
     useEffect(() => {
-        setPayload(prevState => ({
-            ...prevState,
-            "_id": id
-        }));
-
-        if(id){
-            try {
-                GetConfig('calenderacademic', {"_id": id})
+        if (id) {
+            GetConfig('calenderacademic', {"_id": id})
                 .then(res => {
-                    setPayload(detransformJsonLanguage(res[0]))
+                    setPayload(detransformJsonLanguage(res[0]));
                 })
                 .catch((err) => {
                     Swal.fire({
                         allowOutsideClick: false,
                         title: 'Error Notification!',
-                        text: err,
+                        text: err.message || err,
                         icon: 'error',
                     });
-                })
-            } catch (error) {console.log(error);}
+                });
         }
-    }, [])
+    }, [id]);
 
-    return <Suspense>
-        <CalenderAcademicMainForm payload={payload} setPayload={setPayload}/>
-    </Suspense>
+    return (
+        <CalenderAcademicMainForm payload={payload} setPayload={setPayload} />
+    );
+}
+
+const CalenderAcademicFormPage = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <_CalenderAcademicFormPage/>
+        </Suspense>
+    );
 }
 
 export default CalenderAcademicFormPage;
