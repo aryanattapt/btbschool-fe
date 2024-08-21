27c5e4f85c6f74fdb37fdf4cc83c5fe66365a631
Epraldo88 <115973429+Epraldo88@users.noreply.github.com>
2024-08-22 06:54:36 +0700

    styling navbar

--- src/app/_components/navbarapt.jsx
+++ src/app/_components/navbarapt.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { useLanguageStore } from "../../../store/language.store";
 import { NavbarPayload } from "../../../data";
+import { IoIosArrowDown } from "react-icons/io";
 
 const NavBar = () => {
   const [navBarPayload, setNavBarPayload] = useState(NavbarPayload);
@@ -30,13 +31,31 @@ const NavBar = () => {
     setLanguage(e.target.checked ? "EN" : "ID");
   };
 
+  const getGradientSetup = () => {
+    if(isMenuOpen) return 'bg-[#00305E]'
+    else {
+        if(isScrolled) return 'bg-[#00305E]'
+        else return 'bg-gradient-to-b from-[#091531] via-[#00305E] text-[#00305E]'
+        // if(isScrolled){
+        //     if(isMenuOpen) return 'bg-[#00305E]'
+        // } 
+        // return 'bg-gradient-to-b from-[#091531] via-[#00305E] text-[#00305E]'
+
+    }
+  }
+
   return (
     <nav
-      className={`transition-transform duration-500 ease-in-out ${
-        isScrolled ? "bg-[#00305E] text-white" : "bg-gradient-to-b from-[#091531] via-[#00305E] text-[#00305E]"
-      } fixed top-0 w-full p-4 ${
+    //   ${
+    //     (isScrolled && !isMenuOpen) ? "bg-[#00305E]" : "bg-gradient-to-b from-[#091531] via-[#00305E] text-[#00305E]"
+    //   } 
+      className={`transition-transform text-white duration-500 ease-in-out 
+      ${getGradientSetup()}
+      fixed top-0 w-full p-4 ${
         isVisible ? "translate-y-0" : "-translate-y-full"
-      } z-20`}
+      } 
+      
+      z-20`}
     >
         <div className="container mx-auto flex flex-wrap items-center justify-between lg:justify-start px-4">
             {/* Mobile Menu: Logo and Hamburger Button */}
@@ -62,9 +81,9 @@ const NavBar = () => {
             </div>
 
             <div className={`lg:hidden w-full ${isMenuOpen ? "block" : "hidden"} bg-[#00305E]`}>
-                <div className="flex flex-col items-center py-6">
+                <div className="flex flex-col pt-6">
                 {/* Flex container for Language Toggle and Login */}
-                <div className="flex items-center mb-6 space-x-4">
+                <div className="flex items-center mb-2 space-x-4">
                     {/* Language Toggle */}
                     <div className="flex items-center border-r-2 border-gray-400 pr-4">
                     <label className="relative cursor-pointer inline-flex items-center">
@@ -74,7 +93,7 @@ const NavBar = () => {
                         role="switch"
                         id="flexSwitchCheckDefaultMobile"
                         onChange={languageChangeHandler}
-                        className="md:mx-2 mx-1 md:mt-[0.3rem] md:h-3.5 md:w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+                        className="md:mx-2 mx-1 md:mt-[0.3rem] md:h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
                         checked={language === "EN"}
                         />
                         <span className="ml-3 text-white text-lg font-semibold">EN</span>
@@ -103,12 +122,23 @@ const NavBar = () => {
                 </div>
 
                 {/* Navbar Links for Mobile */}
-                <ul className="flex flex-col items-center space-y-6">
+                <ul className="flex flex-col">
                     {navBarPayload[language].navbarlink.map((val, idx) => (
-                    <li key={idx} className="relative group">
-                        <a href={val.url} className="text-white hover:text-[#EF802B] py-3 px-6 block text-lg font-medium">
-                        {val.content}
-                        </a>
+                    <li key={idx} className={`relative group border border-t-0 border-x-0 border-white `}>
+                        <>
+                            {val.submenu ? (
+                                <div className="flex justify-between items-center">
+                                    <p className="text-white hover:text-[#EF802B] py-2 px-6 block text-md font-medium">
+                                        {val.content}
+                                    </p>
+                                    <IoIosArrowDown />
+                                </div>
+                            ) : (
+                                <a className="text-white hover:text-[#EF802B] py-2 px-6 block text-md font-medium">
+                                    {val.content}
+                                </a>
+                            )}
+                        </>
                         {val.submenu && (
                         <ul className="absolute left-0 hidden group-hover:block bg-[#EF802B] text-white mt-2 min-w-[200px] shadow-lg rounded-lg z-50">
                             {val.submenu.map((val2, idx2) => (
@@ -129,7 +159,7 @@ const NavBar = () => {
 
         <div className="hidden lg:flex flex-col container mx-auto lg:flex-row  items-center justify-between w-full p-4">
             {/* Logo */}
-            <div className="flex flex-row w-full justify-between items-center">
+            <div className="flex flex-row w-[390px] justify-between items-center">
                 <a href="/" className="flex items-center">
                 <img
                     src={navBarPayload[language].logourl}
@@ -178,11 +208,11 @@ const NavBar = () => {
                     </div>
                 </div>
 
-                <div className="hidden lg:flex lg:items-center lg:space-x-8"> 
+                <div className="hidden lg:flex lg:items-center justify-end lg:space-x-8"> 
                     {/* Navbar Links */}
-                    <ul className="flex space-x-4">
+                    <ul className="flex">
                         {navBarPayload[language].navbarlink.map((val, idx) => (
-                        <li key={idx} className="relative group">
+                        <li key={idx} className="relative group ">
                             <a href={val.url} className="text-white hover:text-[#EF802B] py-2 px-4 block">
                             {val.content}
                             </a>
