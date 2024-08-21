f0798b74ed2576ca03bf79f284f86bea9bceb3ce
dinaputrikh <dinaputrikh@gmail.com>
2024-08-21 23:43:44 +0700

    ganti gambar carousel btb-belajar tk sd smp sma

--- src/app/(clientpage)/btb-belajar/SD/page.jsx
+++ src/app/(clientpage)/btb-belajar/SD/page.jsx
@@ -1,13 +1,35 @@
+"use client";
 import Banner from "../components/Banner";
 import Pagging from "../components/Pagging";
 import SD from "../components/SD";
+import { useState } from "react";
+import { useLanguageStore } from "../../../../../store/language.store";
+import { BTBBelajarPayload } from "../../../../../data";
+import { Carousel } from "flowbite-react";
 
 const SDPage = () => {
-    return (
-      <>
-        <Banner />
-        <Pagging />
-        <SD />
+  const [btbBelajarData, setBtbBelajarData] = useState(BTBBelajarPayload);
+  const { language } = useLanguageStore();
+  return (
+    <>
+      {/* <Banner /> */}
+      <Carousel
+        slideInterval={12000}
+        className="relative h-[400px] md:h-[600px] lg:h-[1000px] xl:h-[100vh] w-full"
+      >
+        {btbBelajarData[language].sd.bannerImages.map((val, idx) => {
+          return (
+            <div className="relative h-full w-full" key={idx}>
+              <div
+                className="bg-cover absolute inset-0"
+                style={{ backgroundImage: `url(${val})` }}
+              ></div>
+            </div>
+          );
+        })}
+      </Carousel>
+      <Pagging />
+      <SD />
     </>
   );
 };
