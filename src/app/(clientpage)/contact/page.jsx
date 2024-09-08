0d166168d478af7bd6536403da649067ca0972f3
dinaputrikh <dinaputrikh@gmail.com>
2024-09-08 13:20:37 +0700

    update contact page

--- src/app/(clientpage)/contact/page.jsx
+++ src/app/(clientpage)/contact/page.jsx
@@ -15,11 +15,14 @@ const ContactPage = () => {
     <>
       <Banner />
       <Pagging />
-      <div id="hubungi-kami" className="mx-4 md:mx-16 lg:mx-32">
+      <div
+        id="hubungi-kami"
+        className="mx-4 sm:mx-10 md:mx-15 lg:mx-25 xl:mx-32"
+      >
         <h1 className="mt-10 text-[#00305E] text-2xl md:text-4xl lg:text-5xl font-semibold">
           {contactUsData[language].title}
         </h1>
-        <div className="mt-4 text-[#00305E] grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
+        <div className="mt-4 text-[#00305E] grid grid-cols-1 md:grid-cols-3 gap-4 mb-10 place-content-center">
           <div
             id="box"
             className="flex-1 bg-[#00305E] rounded-3xl py-4 md:py-6 lg:py-8 flex flex-col justify-between"
@@ -27,34 +30,45 @@ const ContactPage = () => {
             <div className="text-white">
               {contactUsData[language].schoolList.map((val, idx) => (
                 <div className="px-4 md:px-6 lg:px-8" key={idx}>
-                  <h1 className="font-bold text-lg md:text-xl lg:text-2xl pt-4 md:pt-6 lg:pt-8">
+                  <h1 className="font-bold text-base sm:text-xs md:text-[13px] lg:text-base xl:text-xl 2xl:text-2xl pt-4 md:pt-6 lg:pt-8">
                     {val.title}
                   </h1>
-                  <div className="mt-2 text-sm md:text-base lg:text-lg leading-relaxed">
+                  <h3 className="font-bold text-[16px] sm:text-[18px] md:text-[12px] lg:text-[14px] xl:text-[18px] 2xl:text-[18px] ">
+                    {val.subtitle}
+                  </h3>
+                  <div className="mt-2 text-sm sm:text-sm md:text-[11px] lg:text-base xl:text-lg leading-relaxed">
                     {val.address}
                   </div>
                   <div className="flex items-center mt-2">
                     <HiPhone className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7" />
-                    <p className="ml-2 text-sm md:text-base lg:text-lg">
+                    <p className="ml-2 text-sm sm:text-sm md:text-[11px] lg:text-base xl:text-lg">
                       {val.phoneNumber}
                     </p>
                   </div>
-                  <div className="flex items-center mt-2">
-                    <AiOutlineWhatsApp className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7" />
-                    <a
-                      href={val.hrefwa}
-                      className="ml-2 text-sm md:text-base lg:text-lg"
-                    >
-                      {val.waNumber}
-                    </a>
-                  </div>
+                  {val.waList.map((val1, idx1) => (
+                    <div className="flex items-center mt-2" key={idx1}>
+                      <AiOutlineWhatsApp className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7" />
+                      <a
+                        href={val1.hrefwa}
+                        className="ml-2 text-sm sm:text-sm md:text-[11px] lg:text-base xl:text-lg"
+                      >
+                        {val1.waNumber}
+                      </a>
+                    </div>
+                  ))}
                 </div>
               ))}
-              {/* <div className="py-3 flex items-center text-sm text-gray-800 border-t border-gray-200 dark:text-white dark:border-neutral-600"></div> */}
-              <div className="px-4 md:px-6 lg:px-8 mt-12">
+              <div className="mx-4 my-3 flex items-center text-sm text-gray-800 border-t border-gray-200 dark:text-white dark:border-neutral-600"></div>
+              <div className="px-4 sm:px-4 md:px-6 lg:px-8 xl:px-8 my-2">
                 <div className="flex items-center mt-2">
                   <HiMail className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7" />
-                  <p className="ml-2 text-sm md:text-base lg:text-lg" onClick={() => window.location.href = "mailto:" + contactUsData[language]?.email} >
+                  <p
+                    className="ml-2 text-sm md:text-base lg:text-base xl:text-lg"
+                    onClick={() =>
+                      (window.location.href =
+                        "mailto:" + contactUsData[language]?.email)
+                    }
+                  >
                     {contactUsData[language].email}
                   </p>
                 </div>
@@ -62,12 +76,10 @@ const ContactPage = () => {
             </div>
           </div>
           <div className="col-span-2 flex flex-col">
-            <p className="text-lg md:text-2xl lg:text-3xl my-5 text-black">
+            <p className="text-lg sm:text-lg md:text-xl lg:text-3xl my-5 text-black">
               {contactUsData[language].desc}
             </p>
-            <div className="flex-1">
-              <ContactForm />
-            </div>
+            <div className="flex-1">{/* <ContactForm /> */}</div>
           </div>
         </div>
 
@@ -79,7 +91,10 @@ const ContactPage = () => {
           </h1>
           <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5">
             {contactUsData[language].mapList.map((val, idx) => (
-              <div key={idx} className="py-4 md:py-6 flex flex-col items-center">
+              <div
+                key={idx}
+                className="py-4 md:py-6 flex flex-col items-center"
+              >
                 <h3 className="text-xl md:text-2xl lg:text-3xl text-black font-medium mb-4">
                   {val.title}
                 </h3>
