29ff033c4fa4e9a56e192783bdfd9796e0b4bee3
Aryanatta P.T <aryanattapt28@outlook.co.id>
2024-09-23 21:13:00 +0700

    sort by date bulletin spotlight

--- src/app/(clientpage)/bulletinspotlight/page.jsx
+++ src/app/(clientpage)/bulletinspotlight/page.jsx
@@ -60,7 +60,7 @@ const BulletinSpotlightPage = () => {
 			<Banner />
 			<Pagging />
 			<div className="px-10 py-12 sm:grid sm:grid-cols-2 sm:gap-y-12 sm:gap-x-6 md:gap-12 lg:py-20 lg:px-16 lg:grid-cols-4 lg:gap-8">
-				{payload.map((res, idx) => (
+				{payload.sort((a, b) => new Date(b.date) - new Date(a.date)).map((res, idx) => (
 					<div className={`${idx === 1 && "mt-12 sm:mt-0"}`}>
 						<BulletinThumnail data={res} key={idx} />
 					</div>
