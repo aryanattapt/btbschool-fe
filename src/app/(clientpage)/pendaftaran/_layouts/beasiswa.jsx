359475f6d585fc9d44966d26cc237accf74f0349
dinaputrikh <dinaputrikh@gmail.com>
2024-10-09 22:19:49 +0700

    update pendaftaran page

--- src/app/(clientpage)/pendaftaran/_layouts/beasiswa.jsx
+++ src/app/(clientpage)/pendaftaran/_layouts/beasiswa.jsx
@@ -2,25 +2,28 @@
 import React from "react";
 import { HiPhone, HiMail } from "react-icons/hi";
 import { AiOutlineWhatsApp } from "react-icons/ai";
+/* import { useLanguageStore } from "../../../../../store/language.store"; */
 
-const DetailLayouts = ({ data, language }) => {
+const BeasiswaPage = ({ data, language }) => {
+  /* const { language } = useLanguageStore(); */
+
   return (
     <>
       <div className="md:mx-32 mx-10 text-black leading-loose">
         <div className="mt-10 mb-5 text-[#00305E] sm:justify-center">
           <h1 className="md:text-[35px] text-[25px] font-semibold">
-            {data[language].title}
+            {data[language].beasiswaPagedata.title}
           </h1>
         </div>
         <p className="text-[14px] md:text-[16px] leading-[35px]">
-          {data[language].subtitle}
+          {data[language].beasiswaPagedata.subtitle}
         </p>
         <p className="text-[14px] md:text-[16px] leading-[35px] font-semibold	">
-          {data[language].infomartion}
+          {data[language].beasiswaPagedata.infomartion}
         </p>
 
         <div className="bg-[#EF802B] text-white rounded-xl my-5 px-4 py-7 2xl:w-2/4 xl:w-2/4 sm:w-auto w-auto">
-          {data[language].detailschool.map((val, idx) => {
+          {data[language].beasiswaPagedata.detailschool.map((val, idx) => {
             return (
               <div key={idx}>
                 <h1 className="font-bold text-base sm:text-xs md:text-[13px] lg:text-base xl:text-xl 2xl:text-2xl ">
@@ -68,4 +71,4 @@ const DetailLayouts = ({ data, language }) => {
   );
 };
 
-export default DetailLayouts;
+export default BeasiswaPage;
