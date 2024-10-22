9a41c056e70239c3f7536e4493ab3109a6f404c0
dinaputrikh <dinaputrikh@gmail.com>
2024-10-22 22:21:23 +0700

    update revisi dina

--- src/app/(clientpage)/alumni/_layouts/pagging.jsx
+++ src/app/(clientpage)/alumni/_layouts/pagging.jsx
@@ -1,7 +1,11 @@
 "use client";
 
-const PaggingLayouts = ({ language, alumniPayload, activeTab, setActiveTab }) => {
-
+const PaggingLayouts = ({
+  language,
+  alumniPayload,
+  activeTab,
+  setActiveTab,
+}) => {
   return (
     <>
       <header className="flex flex-row items-start justify-between text-[#00305E] border-b">
@@ -9,9 +13,7 @@ const PaggingLayouts = ({ language, alumniPayload, activeTab, setActiveTab }) =>
           <div className="border-r-2">
             <div className="pr-10 pl-10 md:pr-20 md:pl-32">
               <h1 className="text-[25px] md:text-[25px] text-center font-semibold py-8 md:py-10">
-                <div
-                  className="cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600"
-                >
+                <div className="cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600">
                   ALUMNI
                 </div>
               </h1>
@@ -19,7 +21,7 @@ const PaggingLayouts = ({ language, alumniPayload, activeTab, setActiveTab }) =>
           </div>
           <div className="inline-flex text-[20px] md:text-[20px] text-center text-gray-500 border-gray-200 dark:text-gray-400 dark:border-gray-700">
             <ul className="flex flex-wrap -mb-px py-8 md:py-10">
-              <li className="px-10 sm:px-5 md:px-20 lg:px-4">
+              <li className="px-10 sm:px-5 md:px-20 lg:px-4 border-b-2">
                 <div
                   className={`cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600 ${
                     activeTab === "cerita-alumni"
@@ -31,7 +33,7 @@ const PaggingLayouts = ({ language, alumniPayload, activeTab, setActiveTab }) =>
                   {alumniPayload[language]?.pagingHeader?.url[0]?.title}
                 </div>
               </li>
-              <li className="px-10 sm:px-5 md:px-20 lg:px-4">
+              <li className="px-10 sm:px-5 md:px-20 lg:px-4 border-b-2">
                 <div
                   className={`cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600 ${
                     activeTab === "registertext"
