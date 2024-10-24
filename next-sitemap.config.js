08d49034b7f4326d60b29dc79dbc43ba95001ffb
Aryanatta P.T <aryanattapt28@outlook.co.id>
2024-10-24 18:59:40 +0700

    up production config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-    siteUrl: 'https://alpha.aryanattapt.my.id',
+    siteUrl: 'https://clientpreview.aryanattapt.my.id',
     generateRobotsTxt: true,
-    robotsTxtOptions: {
-        additionalSitemaps: [
-            // You can add additional sitemaps here if needed
-        ],
-        rules: [
-            {
-                userAgent: '*',
-                disallow: ['/admin'], // Disallow any /admin URL
-            },
-        ],
-    },
 }  
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // assetPrefix: "https://aryanattapt.my.id/",
-    // swcMinify: true,
-    // trailingSlash: true,
-    // images: {
-    //     path: 'https://aryanattapt.my.id/_next/image',
-    // },
-    // webpack: (config) => {
-    //     config.resolve.alias.canvas = false;
-    //     return config;
-    // },
+    assetPrefix: "https://clientpreview.aryanattapt.my.id/",
+    swcMinify: false,
+    trailingSlash: true,
+    images: {
+        path: 'https://clientpreview.aryanattapt.my.id/_next/image',
+    },
+    webpack: (config) => {
+        config.resolve.alias.canvas = false;
+        return config;
+    },    
 };
 
 export default nextConfig;
