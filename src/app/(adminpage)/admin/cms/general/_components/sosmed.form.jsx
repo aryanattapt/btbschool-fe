0110e9ee68a6fb9c5664aaeb4e1849b65a9f85d9
dinaputrikh <dinaputrikh@gmail.com>
2024-11-23 21:04:20 +0700

    update point 3.2 and 6

--- src/app/(adminpage)/admin/cms/general/_components/sosmed.form.jsx
+++ src/app/(adminpage)/admin/cms/general/_components/sosmed.form.jsx
@@ -1,136 +1,165 @@
-'use client'
-import { 
-    Button,
-    FileInput,
-} from "flowbite-react";
-import AgGridTableForm from '../../_components/aggrid.form';
+"use client";
+import { Button, FileInput } from "flowbite-react";
+import AgGridTableForm from "../../_components/aggrid.form";
 
-const SosialMediaForm = ({formChangeHandler, payload}) => {
-    const LogoFileInputRenderer = (params) => {
-        const handleFileChange = (event) => {
-            const file = event.target.files[0];
-            if (file  && file.size <= 2 * 1024 * 1024 && file.type.startsWith('image/')) {
-                params.setValue(file.name);
-    
-                /* Jika mau disave jadi file beneran */
-                /* params.node.data.file = file;
+const SosialMediaForm = ({ formChangeHandler, payload }) => {
+  const LogoFileInputRenderer = (params) => {
+    const handleFileChange = (event) => {
+      const file = event.target.files[0];
+      if (
+        file &&
+        file.size <= 2 * 1024 * 1024 &&
+        file.type.startsWith("image/")
+      ) {
+        params.setValue(file.name);
+
+        /* Jika mau disave jadi file beneran */
+        /* params.node.data.file = file;
                 params.api.refreshCells({ rowNodes: [params.node] }); */
-    
-                /* Disimpan sebagai base64 */
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                    params.node.data[params.column.colId] = reader.result;
-                };
-                reader.readAsDataURL(file);
-                params.api.refreshCells({ rowNodes: [params.node] });
-            } else{
-                alert(`${file.name} exceeds the 2 MB size limit or file should be valid image format`);
-            }
+
+        /* Disimpan sebagai base64 */
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          params.node.data[params.column.colId] = reader.result;
         };
-    
-        return <FileInput accept="image/*" name={params.column.colid} onChange={handleFileChange} />;
+        reader.readAsDataURL(file);
+        params.api.refreshCells({ rowNodes: [params.node] });
+      } else {
+        alert(
+          `${file.name} exceeds the 2 MB size limit or file should be valid image format`
+        );
+      }
     };
-    
-    const DeleteButtonRenderer = (params, formChangeHandler, name) => {
-        const handleDelete = () => {
-            params.api.applyTransaction({ remove: [params.node.data] });
-    
-            let gridData = [];
-            params.api.forEachNode(node => gridData.push(node.data));
-            formChangeHandler({
-                "target": {
-                    "name": name,
-                    "value": gridData,
-                    "type": "table"
-                }
-            });
-        };
-    
-        return <Button onClick={handleDelete} color={"failure"}>Delete</Button>;
+
+    return (
+      <FileInput
+        accept="image/*"
+        name={params.column.colid}
+        onChange={handleFileChange}
+      />
+    );
+  };
+
+  const DeleteButtonRenderer = (params, formChangeHandler, name) => {
+    const handleDelete = () => {
+      params.api.applyTransaction({ remove: [params.node.data] });
+
+      let gridData = [];
+      params.api.forEachNode((node) => gridData.push(node.data));
+      formChangeHandler({
+        target: {
+          name: name,
+          value: gridData,
+          type: "table",
+        },
+      });
     };
-    
-    const columnDefs = [
-        {
-            headerName: "Name",
-            field: "name",
-            filter: false,
-            sortable: false,
-            resizable: false,
-            suppressHeaderMenuButton: true,
-            suppressMovable: true,
-            enableRowGroup: false,
-            suppressAutoSize: true,
-            suppressSizeToFit: true,
-            headerClass: 'cell-center',
-            cellStyle: { textAlign: 'left' },
-            width: "200vw",
-            editable: true,
-        },
-        {
-            headerName: "Logo",
-            field: "logo",
-            filter: false,
-            sortable: false,
-            resizable: false,
-            suppressHeaderMenuButton: true,
-            suppressMovable: true,
-            enableRowGroup: false,
-            suppressAutoSize: true,
-            suppressSizeToFit: true,
-            headerClass: 'cell-center',
-            cellStyle: { textAlign: 'left' },
-            width: "140vw",
-            editable: true,
-            cellRenderer: (p) => LogoFileInputRenderer(p),
-        },
-        {
-            headerName: "Link",
-            field: "link",
-            filter: false,
-            sortable: false,
-            resizable: false,
-            suppressHeaderMenuButton: true,
-            suppressMovable: true,
-            enableRowGroup: false,
-            suppressAutoSize: true,
-            suppressSizeToFit: true,
-            headerClass: 'cell-center',
-            cellStyle: { textAlign: 'left' },
-            width: "1000vw",
-            editable: true,
-        },
-        {
-            headerName: "Delete",
-            field: "deleteAction",
-            cellRenderer: (p) => DeleteButtonRenderer(p, formChangeHandler, 'socialmedia'),
-            width: "100vw",
-            filter: false,
-            sortable: false,
-            resizable: false,
-            suppressHeaderMenuButton: true,
-            suppressMovable: true,
-            enableRowGroup: false,
-            suppressAutoSize: true,
-            suppressSizeToFit: true,
-            headerClass: 'cell-center',
-            cellStyle: { textAlign: 'center' },
-        },
-    ];
-    
-    return <>
-        <div className="mt-3 w-fit font-semibold text-[15px] text-[#00305E] border-b-8 border-b border-[#EF802B]">
-            {`Sosial Media`}
+
+    return (
+      <Button onClick={handleDelete} color={"failure"}>
+        Delete
+      </Button>
+    );
+  };
+
+  const columnDefs = [
+    {
+      headerName: "Name",
+      field: "name",
+      filter: false,
+      sortable: false,
+      resizable: false,
+      suppressHeaderMenuButton: true,
+      suppressMovable: true,
+      enableRowGroup: false,
+      suppressAutoSize: true,
+      suppressSizeToFit: true,
+      headerClass: "cell-center",
+      cellStyle: { textAlign: "left" },
+      width: "200vw",
+      editable: true,
+    },
+    {
+      headerName: "Logo",
+      field: "logo",
+      filter: false,
+      sortable: false,
+      resizable: false,
+      suppressHeaderMenuButton: true,
+      suppressMovable: true,
+      enableRowGroup: false,
+      suppressAutoSize: true,
+      suppressSizeToFit: true,
+      headerClass: "cell-center",
+      cellStyle: { textAlign: "left" },
+      width: "140vw",
+      editable: true,
+      cellRenderer: (p) => LogoFileInputRenderer(p),
+    },
+    {
+      headerName: "Link",
+      field: "link",
+      filter: false,
+      sortable: false,
+      resizable: false,
+      suppressHeaderMenuButton: true,
+      suppressMovable: true,
+      enableRowGroup: false,
+      suppressAutoSize: true,
+      suppressSizeToFit: true,
+      headerClass: "cell-center",
+      cellStyle: { textAlign: "left" },
+      width: "1000vw",
+      editable: true,
+    },
+    {
+      headerName: "Delete",
+      field: "deleteAction",
+      cellRenderer: (p) =>
+        DeleteButtonRenderer(p, formChangeHandler, "socialmedia"),
+      width: "100vw",
+      filter: false,
+      sortable: false,
+      resizable: false,
+      suppressHeaderMenuButton: true,
+      suppressMovable: true,
+      enableRowGroup: false,
+      suppressAutoSize: true,
+      suppressSizeToFit: true,
+      headerClass: "cell-center",
+      cellStyle: { textAlign: "center" },
+    },
+  ];
+
+  return (
+    <>
+      <div className="mt-3 w-fit font-semibold text-[15px] text-[#00305E] border-b-8 border-b border-[#EF802B]">
+        {`Sosial Media`}
+      </div>
+      <div className="mt-3 w-fit">
+        <p className="mt-1 text-sm text-gray-500">Ukuran Maksimum Logo 2 MB.</p>
+        <p className="mt-1 text-sm text-gray-500">
+          Format Image dengan aspect ratio 1:1 contoh 24px x 24px
+        </p>
+        <p className="mt-1 text-sm text-gray-500">
+          Format Link berbentuk URL (https://test.com)
+        </p>
+      </div>
+      <div className="mt-2 mb-10">
+        <div
+          className="ag-theme-quartz pr-10 md:pr-0"
+          style={{ height: "200px", width: "100%" }}
+        >
+          <AgGridTableForm
+            name="socialmedia"
+            formChangeHandler={formChangeHandler}
+            payload={payload?.socialmedia || []}
+            columnDefs={columnDefs}
+          />
         </div>
-        <div className="mt-3 w-fit">
-            <p className="mt-1 text-sm text-gray-500">Ukuran Maksimum Logo 2 MB.</p>
-            <p className="mt-1 text-sm text-gray-500">Format Link berbentuk URL (https://test.com)</p>
-        </div>
-        <div className="mt-2 mb-10">
-            <div className="ag-theme-quartz pr-10 md:pr-0" style={{height: '200px', width: "100%"}}>
-                <AgGridTableForm name="socialmedia" formChangeHandler={formChangeHandler} payload={payload?.socialmedia || []}  columnDefs={columnDefs}/>
-            </div>
-        </div>
+      </div>
     </>
-}
+  );
+};
 
 export default SosialMediaForm;
--- src/app/(clientpage)/alumni/_layouts/cerita-alumni.jsx
+++ src/app/(clientpage)/alumni/_layouts/cerita-alumni.jsx
@@ -1,147 +1,159 @@
 "use client";
 import React from "react";
 import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import './sliderStyles.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./sliderStyles.css";
 
 const CustomPrevArrow = (props) => {
-    const { onClick } = props;
-    return (
-        <button className="custom-prev-arrow" onClick={onClick}>
-            &#10094;
-        </button>
-    );
+  const { onClick } = props;
+  return (
+    <button className="custom-prev-arrow" onClick={onClick}>
+      &#10094;
+    </button>
+  );
 };
 
 const CustomNextArrow = (props) => {
-    const { onClick } = props;
-    return (
-        <button className="custom-next-arrow" onClick={onClick}>
-            &#10095;
-        </button>
-    );
+  const { onClick } = props;
+  return (
+    <button className="custom-next-arrow" onClick={onClick}>
+      &#10095;
+    </button>
+  );
 };
 
+const CeritaAlumni = ({
+  alumniPayload,
+  alumniStoryPayload,
+  alumniUniversityPayload,
+  language,
+}) => {
+  const settingsTestimonies = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
 
-const CeritaAlumni = ({ alumniPayload, alumniStoryPayload, alumniUniversityPayload, language }) => {
-    const settingsTestimonies = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: <CustomPrevArrow />,
-        nextArrow: <CustomNextArrow />,
-      };
-    
-    // const settingsFlag = {
-    //     dots: true, // Enable dot indicators
-    //     infinite: true, // Infinite scrolling
-    //     speed: 500, // Transition speed
-    //     slidesToShow: 3, // Number of slides to show at once
-    //     slidesToScroll: 1, // Number of slides to scroll
-    //     responsive: [
-    //         {
-    //             breakpoint: 1024,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //             },
-    //         },
-    //         {
-    //             breakpoint: 600,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //             },
-    //         },
-    //     ],
-    // };
+  // const settingsFlag = {
+  //     dots: true, // Enable dot indicators
+  //     infinite: true, // Infinite scrolling
+  //     speed: 500, // Transition speed
+  //     slidesToShow: 3, // Number of slides to show at once
+  //     slidesToScroll: 1, // Number of slides to scroll
+  //     responsive: [
+  //         {
+  //             breakpoint: 1024,
+  //             settings: {
+  //                 slidesToShow: 2,
+  //             },
+  //         },
+  //         {
+  //             breakpoint: 600,
+  //             settings: {
+  //                 slidesToShow: 1,
+  //             },
+  //         },
+  //     ],
+  // };
 
-    const settingsFlag = {
-        dots: false,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        speed: 1000,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: false,
-    };
+  const settingsFlag = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+  };
 
   return (
     <div className="px-12 pb-12">
-        <div className="flex flex-col items-start">
-            <h2 className="text-[#00305E] text-[25px] md:text-[50px] font-semibold mx-5 my-5">
-                {alumniPayload[language]?.pagingHeader?.url[0]?.title}
-            </h2>
-        </div>
-        <div className="slider-container">
+      <div className="flex flex-col items-start">
+        <h2 className="text-[#00305E] text-[25px] md:text-[50px] font-semibold mx-5 my-5">
+          {alumniPayload[language]?.pagingHeader?.url[0]?.title}
+        </h2>
+      </div>
+      <div className="slider-container">
         <div className="bg-slate-300 rounded mb-20">
-        {/* <div className="bg-slate-300 rounded mb-20 min-h-[20rem] sm:min-h-[25rem] md:min-h-[30rem] lg:min-h-[35rem] xl:min-h-[40rem]"> */}
-            <Slider {...settingsTestimonies}>
-                {alumniStoryPayload?.map((val, idx) => {
-                    return(
-                    <div key={idx}>
-                        <div className="p-10 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2">
-                            {
-                                val?.image && <div className="md:px-10">
-                                    <img
-                                        src={val?.image}
-                                        alt="alumni"
-                                        className="xl:w-[1124px] md:w-auto rounded-md"
-                                    />
-                                </div>
-                            }
-                            <div className="text-[14px] md:text-[20px] lg:text-[25px] xl:text-[18px]">
-                                <div className="flex">
-                                    <h1 className="text-[#00305E] font-bold text-[14px] md:text-[20px] lg:text-[25px] xl:text-[25px] pr-2 max-w-1/2">{val?.name}</h1>
-                                    <h1 className="text-gray-500 font-bold text-[14px] md:text-[20px] lg:text-[25px] xl:text-[25px] w-1/2 content-center">Class of {val?.class}</h1>
-                                </div>
-                                <div className="mr-10">
-                                    <p className="font-semibold">{val?.university}, {val?.major}</p>
-                                    <p>{val?.testimonies}</p>
-                                </div>
-                            </div>
-                        </div>
+          {/* <div className="bg-slate-300 rounded mb-20 min-h-[20rem] sm:min-h-[25rem] md:min-h-[30rem] lg:min-h-[35rem] xl:min-h-[40rem]"> */}
+          <Slider {...settingsTestimonies}>
+            {alumniStoryPayload?.map((val, idx) => {
+              return (
+                <div key={idx}>
+                  <div className="p-10 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2">
+                    {val?.image && (
+                      <div className="md:px-10">
+                        <img
+                          src={val?.image}
+                          alt="alumni"
+                          className="xl:w-[1124px] md:w-auto rounded-md"
+                        />
+                      </div>
+                    )}
+                    <div className="text-[14px] md:text-[20px] lg:text-[25px] xl:text-[18px] place-content-center">
+                      <div className="flex">
+                        <h1 className="text-[#00305E] font-bold text-[14px] md:text-[20px] lg:text-[25px] xl:text-[25px] pr-2 max-w-1/2">
+                          {val?.name}
+                        </h1>
+                        <h1 className="text-gray-500 font-bold text-[14px] md:text-[20px] lg:text-[25px] xl:text-[25px] w-1/2 content-center">
+                          Class of {val?.class}
+                        </h1>
+                      </div>
+                      <div className="xl:mr-10 lg:mr-10 mt-4">
+                        <p className="font-semibold text-[12px] md:text-[17px] lg:text-[18px] xl:text-[18px]">
+                          {val?.university}, {val?.major}
+                        </p>
+                        <p className="my-3 xl:mt-3 text-[11px] md:text-[16px] lg:text-[17px] xl:text-[17px]">
+                          {val?.testimonies}
+                        </p>
+                      </div>
                     </div>
-                    );
-                })}
-            </Slider>
-            </div>
+                  </div>
+                </div>
+              );
+            })}
+          </Slider>
         </div>
+      </div>
 
-        <div className="my-5 text-[14px] md:text-[20px] lg:text-[25px] xl:text-[18px]">
-            <h2 className="text-[#00305E] text-[25px] md:text-[50px] mx-5">
-                {/* {alumniPayload[language]?.pagingHeader?.url[0]?.subtitle} */}
-                {alumniPayload[language]?.universitytitle}
-            </h2>
-            <p className="mx-5 mb-5">
-                {/* Kami berkomitmen mempersiapkan setiap murid untuk meraih sukses di universitas terkemuka, baik di dalam maupun luar negeri.
+      <div className="my-5 text-[14px] md:text-[20px] lg:text-[25px] xl:text-[18px]">
+        <h2 className="text-[#00305E] text-[25px] md:text-[50px] mx-5">
+          {/* {alumniPayload[language]?.pagingHeader?.url[0]?.subtitle} */}
+          {alumniPayload[language]?.universitytitle}
+        </h2>
+        <p className="mx-5 mb-5">
+          {/* Kami berkomitmen mempersiapkan setiap murid untuk meraih sukses di universitas terkemuka, baik di dalam maupun luar negeri.
                 Dengan kurikulum berstandar internasional, pendampingan akademis yang menyeluruh, serta bimbingan personal yang mendalam, 
                 kami memastikan setiap siswa siap menghadapi tantangan pendidikan tinggi dengan percaya diri dan kompetensi global. */}
-                {alumniPayload[language]?.universitycontent}
-            </p>
-        </div>
-        <div className="slider-container">
-            <Slider {...settingsFlag}>
-                {alumniUniversityPayload?.map((val, idx) => {
-                    return(
-                        <div key={idx} className="relative">
-                            {
-                                val?.image && <img
-                                    src={val.image}
-                                    alt="alumni"
-                                    className="w-[400px] h-[200px] object-contain"
-                                />
-                            }
-                            <span className="absolute inset-0 flex items-center justify-center text-white bg-black bg-opacity-50">
-                                {val?.caption}
-                            </span>
-                        </div>
-                    );
-                })}
-            </Slider>
-        </div>
+          {alumniPayload[language]?.universitycontent}
+        </p>
+      </div>
+      <div className="slider-container">
+        <Slider {...settingsFlag}>
+          {alumniUniversityPayload?.map((val, idx) => {
+            return (
+              <div key={idx} className="relative">
+                {val?.image && (
+                  <img
+                    src={val.image}
+                    alt="alumni"
+                    className="w-[400px] h-[200px] object-contain"
+                  />
+                )}
+                <span className="absolute inset-0 flex items-center justify-center text-white bg-black bg-opacity-50">
+                  {val?.caption}
+                </span>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
     </div>
   );
 };
