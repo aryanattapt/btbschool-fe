95a579700a1e79fc60381e459e0f466034fb1395
Epraldo88 <115973429+Epraldo88@users.noreply.github.com>
2025-02-11 20:50:09 +0700

    add edit feature for Calendar

--- src/app/(clientpage)/calendar-academic/page.jsx
+++ src/app/(clientpage)/calendar-academic/page.jsx
@@ -1,29 +1,39 @@
-'use client'
-import { useEffect } from 'react';
-import Banner from "./components/banner";
-import Pagging from "./components/pagging";
-import Content from "./components/content";
-import { usePageData } from '../../../hooks/usePageData';
+"use client";
+import { useEffect } from "react";
+import { usePageData } from "../../../hooks/usePageData";
 import Loader from "../../_components/loader";
+import Banner from "./components/banner";
+import Content from "./components/content";
+import Pagging from "./components/pagging";
 
 const CalendarAcademicPage = () => {
-  const {language, getCalenderAcademicPageData, isLoading} = usePageData();
-  const payload = usePageData((state) => state.result.calenderacademic);
-  const calendarAcademicData = usePageData((state) => state.result.CalendarAcademicPageData);
+	const { language, getCalenderAcademicPageData, isLoading } = usePageData();
+	const payload = usePageData((state) => state.result.calenderacademic);
+	const calendarAcademicData = usePageData(
+		(state) => state.result.CalendarAcademicPageData
+	);
 
-  useEffect(() => {
-    getCalenderAcademicPageData();
-  }, []);
+	useEffect(() => {
+		getCalenderAcademicPageData();
+	}, []);
 
-  if(isLoading) {
-    return <Loader/>;
-  }
-  else if(payload)
-    return <>
-      <Banner calenderPayload={calendarAcademicData}/>
-      <Pagging calendarAcademicData={calendarAcademicData} language={language}/>
-      <Content payload={payload} content={calendarAcademicData[language]} language={language}/>
-    </>
+	if (isLoading) {
+		return <Loader />;
+	} else if (payload)
+		return (
+			<>
+				<Banner calenderPayload={calendarAcademicData} />
+				<Pagging
+					calendarAcademicData={calendarAcademicData}
+					language={language}
+				/>
+				<Content
+					payload={payload}
+					content={calendarAcademicData[language]}
+					language={language}
+				/>
+			</>
+		);
 };
 
 export default CalendarAcademicPage;
