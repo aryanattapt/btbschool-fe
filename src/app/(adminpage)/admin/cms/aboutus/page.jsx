b85e6ac95856802909d6cb150c9bfe2a15a84351
Epraldo88 <eko.prasetyo1314@gmail.com>
2025-03-14 11:55:19 +0700

    add motto and its cms

--- src/app/(adminpage)/admin/cms/aboutus/page.jsx
+++ src/app/(adminpage)/admin/cms/aboutus/page.jsx
@@ -15,128 +15,129 @@ import Swal from "sweetalert2";
 import LoadingModal from "../../../../../components/LoadingModal";
 
 const CMSAboutUs = () => {
-	const [isLoadingPage, setIsLoadingPage] = useState(true);
-	const [isAuthorized, setIsAuthorized] = useState(null);
+  const [isLoadingPage, setIsLoadingPage] = useState(true);
+  const [isAuthorized, setIsAuthorized] = useState(null);
 
-	const rawData = useCmsAboutUsStore((state) => state.rawData);
-	const setState = useCmsAboutUsStore((state) => state.setState);
-	const language = useCmsAboutUsStore((state) => state.language);
-	const getInitialData = useCmsAboutUsStore((state) => state.getInitialData);
-	const setDescription = useCmsAboutUsStore((state) => state.setDescription);
-	const setVisiMisi = useCmsAboutUsStore((state) => state.setVisiMisi);
-	const deleteVisiMisi = useCmsAboutUsStore((state) => state.deleteVisiMisi);
-	const addVisiMisi = useCmsAboutUsStore((state) => state.addVisiMisi);
-	const setSmallParagraph = useCmsAboutUsStore(
-		(state) => state.setSmallParagraph
-	);
-	const data = useCmsAboutUsStore((state) => state.data);
-	const submitData = useCmsAboutUsStore((state) => state.submitData);
-	const loading = useCmsAboutUsStore((state) => state.loading);
-	// const onChangeAttachment = useCmsAboutUsStore(
-	// 	(state) => state.onChangeAttachment
-	// );
+  const rawData = useCmsAboutUsStore((state) => state.rawData);
+  const setState = useCmsAboutUsStore((state) => state.setState);
+  const language = useCmsAboutUsStore((state) => state.language);
+  const getInitialData = useCmsAboutUsStore((state) => state.getInitialData);
+  const setDescription = useCmsAboutUsStore((state) => state.setDescription);
+  const setVisiMisi = useCmsAboutUsStore((state) => state.setVisiMisi);
+  const deleteVisiMisi = useCmsAboutUsStore((state) => state.deleteVisiMisi);
+  const addVisiMisi = useCmsAboutUsStore((state) => state.addVisiMisi);
+  const setSmallParagraph = useCmsAboutUsStore(
+    (state) => state.setSmallParagraph
+  );
+  const setMotto = useCmsAboutUsStore((state) => state.setMotto);
+  const data = useCmsAboutUsStore((state) => state.data);
+  const submitData = useCmsAboutUsStore((state) => state.submitData);
+  const loading = useCmsAboutUsStore((state) => state.loading);
+  // const onChangeAttachment = useCmsAboutUsStore(
+  // 	(state) => state.onChangeAttachment
+  // );
 
-	const [attachment, setAttachment] = useState({});
+  const [attachment, setAttachment] = useState({});
 
-	useEffect(() => {
-		fetchData(getInitialData);
-	}, []);
+  useEffect(() => {
+    fetchData(getInitialData);
+  }, []);
 
-	const fetchData = async (callback) => {
-		setIsLoadingPage(true);
-		try {
-			await checkPermission("manage_content");
-			setIsAuthorized(true);
-			await callback();
-		} catch (error) {
-			console.log(error);
-			if (error.status != "401") {
-				try {
-					await callback();
-				} catch (error) {
-					console.log(error);
-				}
-			}
-		} finally {
-			setIsLoadingPage(false);
-		}
-	};
+  const fetchData = async (callback) => {
+    setIsLoadingPage(true);
+    try {
+      await checkPermission("manage_content");
+      setIsAuthorized(true);
+      await callback();
+    } catch (error) {
+      console.log(error);
+      if (error.status != "401") {
+        try {
+          await callback();
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    } finally {
+      setIsLoadingPage(false);
+    }
+  };
 
-	const onChangeAttachment = (file, prop) => {
-		if (file.length > 0) {
-			attachment[prop] = file[0];
-		} else {
-			// setState("", prop);
-			attachment[prop] = rawData[prop];
-		}
-		setAttachment({ ...attachment });
-	};
+  const onChangeAttachment = (file, prop) => {
+    if (file.length > 0) {
+      attachment[prop] = file[0];
+    } else {
+      // setState("", prop);
+      attachment[prop] = rawData[prop];
+    }
+    setAttachment({ ...attachment });
+  };
 
-	useEffect(() => {
-		if (!isObjectEmpty(rawData)) {
-			setAttachment({
-				image1: rawData["image1"],
-				image2: rawData["image2"],
-				image3: rawData["image3"],
-				image4: rawData["image4"],
-				image5: rawData["image5"],
-				image6: rawData["image6"],
-			});
-		}
-	}, [rawData]);
+  useEffect(() => {
+    if (!isObjectEmpty(rawData)) {
+      setAttachment({
+        image1: rawData["image1"],
+        image2: rawData["image2"],
+        image3: rawData["image3"],
+        image4: rawData["image4"],
+        image5: rawData["image5"],
+        image6: rawData["image6"],
+      });
+    }
+  }, [rawData]);
 
-	const onSubmitData = () => {
-		Swal.fire(
-			"Are you sure?",
-			"Once submitted, you can't undo it",
-			"warning"
-		).then((res) => {
-			if (res.isConfirmed) {
-				setState(true, "loading");
-				const container = {};
-				Object.keys(attachment).forEach((key) => {
-					if (typeof attachment[key] === "object")
-						container[key] = attachment[key];
-				});
-				submitData(container);
-			}
-		});
-	};
+  const onSubmitData = () => {
+    Swal.fire(
+      "Are you sure?",
+      "Once submitted, you can't undo it",
+      "warning"
+    ).then((res) => {
+      if (res.isConfirmed) {
+        setState(true, "loading");
+        const container = {};
+        Object.keys(attachment).forEach((key) => {
+          if (typeof attachment[key] === "object")
+            container[key] = attachment[key];
+        });
+        submitData(container);
+      }
+    });
+  };
 
-	if (isLoadingPage) {
-		return <Loader />;
-	} else
-		return (
-			<NavbarSidebarLayout>
-				{isAuthorized ? (
-					<>
-						{!isObjectEmpty(data) && (
-							<div>
-								<AdminHeader title="About Us Content Settings Form" />
-								<FieldTitle>Gambar Banner</FieldTitle>
-								<ImageAttachment
-									resolution="dengan menggunakan aspect ratio wide Misal 1920x1080px"
-									id="image1"
-									onChange={(e) =>
-										onChangeAttachment(e.target.files, "bannerimage")
-									}
-								/>
+  if (isLoadingPage) {
+    return <Loader />;
+  } else
+    return (
+      <NavbarSidebarLayout>
+        {isAuthorized ? (
+          <>
+            {!isObjectEmpty(data) && (
+              <div>
+                <AdminHeader title="About Us Content Settings Form" />
+                <FieldTitle>Gambar Banner</FieldTitle>
+                <ImageAttachment
+                  resolution="dengan menggunakan aspect ratio wide Misal 1920x1080px"
+                  id="image1"
+                  onChange={(e) =>
+                    onChangeAttachment(e.target.files, "bannerimage")
+                  }
+                />
 
-								{/* Pengenanlan */}
-								<FieldTitle>Gambar Pengenalan</FieldTitle>
-								<ImageAttachment
-									resolution="dengan menggunakan aspect ratio wide Misal 1920x1080px"
-									id="image1"
-									onChange={(e) => onChangeAttachment(e.target.files, "image1")}
-								/>
-								<FieldTitle>Gambar Visi Misi</FieldTitle>
-								<ImageAttachment
-									resolution="dengan menggunakan aspect ratio wide Misal 1920x1080px"
-									id="image2"
-									onChange={(e) => onChangeAttachment(e.target.files, "image2")}
-								/>
+                {/* Pengenanlan */}
+                <FieldTitle>Gambar Pengenalan</FieldTitle>
+                <ImageAttachment
+                  resolution="dengan menggunakan aspect ratio wide Misal 1920x1080px"
+                  id="image1"
+                  onChange={(e) => onChangeAttachment(e.target.files, "image1")}
+                />
+                <FieldTitle>Gambar Visi Misi</FieldTitle>
+                <ImageAttachment
+                  resolution="dengan menggunakan aspect ratio wide Misal 1920x1080px"
+                  id="image2"
+                  onChange={(e) => onChangeAttachment(e.target.files, "image2")}
+                />
 
-								{/* <FieldTitle>List Grade</FieldTitle>
+                {/* <FieldTitle>List Grade</FieldTitle>
 						{data["ID"]["gradelists"].map((res, index) => (
 							<div className="mb-3" key={index}>
 								<h6>{res.title}</h6>
@@ -148,72 +149,79 @@ const CMSAboutUs = () => {
 								/>
 							</div>
 						))} */}
-								<div className="mt-6">
-									<LanguageChanger
-										onChange={(val) => setState(val, "language")}
-										value={language}
-									/>
-									<FieldTitle>Pengenalan</FieldTitle>
-									<Textarea
-										rows={4}
-										value={data[language]["desc"]}
-										onChange={(e) => {
-											setDescription(e.target.value);
-										}}
-									/>
-									<FieldTitle>Deskripsi Visi</FieldTitle>
-									<TextInput
-										value={data[language]["visimisi"]["descvisi"]}
-										onChange={(e) => {
-											setVisiMisi(e.target.value, "descvisi");
-										}}
-									/>
-									<FieldTitle>Deskripsi Misi</FieldTitle>
-									<TextInput
-										value={data[language]["visimisi"]["descmisi"]}
-										onChange={(e) => {
-											setVisiMisi(e.target.value, "descmisi");
-										}}
-									/>
-									<FieldTitle>List Misi</FieldTitle>
-									<div className="flex flex-col gap-2">
-										{data[language]["visimisi"]["misilist"].map(
-											(res, index) => (
-												<div className="flex w-full items-center">
-													<div
-														onClick={() => deleteVisiMisi(index)}
-														className="mr-4 cursor-pointer text-xl text-red-600 hover:text-red-700"
-													>
-														<FaMinusCircle />
-													</div>
-													<div className="w-full">
-														<TextInput
-															value={res}
-															onChange={(e) => {
-																setVisiMisi(e.target.value, "misilist", index);
-															}}
-														/>
-													</div>
-												</div>
-											)
-										)}
-									</div>
-									<Button
-										className="mt-2 ml-9"
-										onClick={addVisiMisi}
-										size={"sm"}
-									>
-										Add
-									</Button>
-									<FieldTitle>Catatan Kaki</FieldTitle>
-									<Textarea
-										rows={4}
-										value={data[language]["smallparagraph"]}
-										onChange={(e) => {
-											setSmallParagraph(e.target.value, "smallparagraph");
-										}}
-									/>
-									{/* <FieldTitle>List Jenjang Pendidikan</FieldTitle>
+                <div className="mt-6">
+                  <LanguageChanger
+                    onChange={(val) => setState(val, "language")}
+                    value={language}
+                  />
+                  <FieldTitle>Pengenalan</FieldTitle>
+                  <Textarea
+                    rows={4}
+                    value={data[language]["desc"]}
+                    onChange={(e) => {
+                      setDescription(e.target.value);
+                    }}
+                  />
+                  <FieldTitle>Motto</FieldTitle>
+                  <TextInput
+                    value={data[language]?.["motto"]}
+                    onChange={(e) => {
+                      setMotto(e.target.value);
+                    }}
+                  />
+                  <FieldTitle>Deskripsi Visi</FieldTitle>
+                  <TextInput
+                    value={data[language]["visimisi"]["descvisi"]}
+                    onChange={(e) => {
+                      setVisiMisi(e.target.value, "descvisi");
+                    }}
+                  />
+                  <FieldTitle>Deskripsi Misi</FieldTitle>
+                  <TextInput
+                    value={data[language]["visimisi"]["descmisi"]}
+                    onChange={(e) => {
+                      setVisiMisi(e.target.value, "descmisi");
+                    }}
+                  />
+                  <FieldTitle>List Misi</FieldTitle>
+                  <div className="flex flex-col gap-2">
+                    {data[language]["visimisi"]["misilist"].map(
+                      (res, index) => (
+                        <div className="flex w-full items-center">
+                          <div
+                            onClick={() => deleteVisiMisi(index)}
+                            className="mr-4 cursor-pointer text-xl text-red-600 hover:text-red-700"
+                          >
+                            <FaMinusCircle />
+                          </div>
+                          <div className="w-full">
+                            <TextInput
+                              value={res}
+                              onChange={(e) => {
+                                setVisiMisi(e.target.value, "misilist", index);
+                              }}
+                            />
+                          </div>
+                        </div>
+                      )
+                    )}
+                  </div>
+                  <Button
+                    className="mt-2 ml-9"
+                    onClick={addVisiMisi}
+                    size={"sm"}
+                  >
+                    Add
+                  </Button>
+                  <FieldTitle>Catatan Kaki</FieldTitle>
+                  <Textarea
+                    rows={4}
+                    value={data[language]["smallparagraph"]}
+                    onChange={(e) => {
+                      setSmallParagraph(e.target.value, "smallparagraph");
+                    }}
+                  />
+                  {/* <FieldTitle>List Jenjang Pendidikan</FieldTitle>
 									<Textarea
 										rows={4}
 										value={data[language]["smallparagraph"]}
@@ -221,24 +229,24 @@ const CMSAboutUs = () => {
 											setVisiMisi(e.target.value, "smallparagraph");
 										}}
 									/> */}
-									<Button
-										id="btnSaveAndSend"
-										name="btnSaveAndSend"
-										className="w-full md:w-auto mt-3"
-										onClick={onSubmitData}
-									>
-										Save
-									</Button>
-								</div>
-							</div>
-						)}
-					</>
-				) : (
-					<div>Unauthorized</div>
-				)}
-				{loading && <LoadingModal label={"Submitting data, please wait..."} />}
-			</NavbarSidebarLayout>
-		);
+                  <Button
+                    id="btnSaveAndSend"
+                    name="btnSaveAndSend"
+                    className="w-full md:w-auto mt-3"
+                    onClick={onSubmitData}
+                  >
+                    Save
+                  </Button>
+                </div>
+              </div>
+            )}
+          </>
+        ) : (
+          <div>Unauthorized</div>
+        )}
+        {loading && <LoadingModal label={"Submitting data, please wait..."} />}
+      </NavbarSidebarLayout>
+    );
 };
 
 export default CMSAboutUs;
--- store/admin/cms/aboutUsStore.js
+++ store/admin/cms/aboutUsStore.js
@@ -6,109 +6,114 @@ import { UploadAttachment } from "../../../services/attachment.service";
 import Swal from "sweetalert2";
 
 const initialData = {
-	rawData: {},
-	data: {},
-	language: "ID",
-	loading: false,
+  rawData: {},
+  data: {},
+  language: "ID",
+  loading: false,
 };
 
 const template = (get) => {
-	return {
-		data: deepCopy(get().data),
-		language: get().language,
-	};
+  return {
+    data: deepCopy(get().data),
+    language: get().language,
+  };
 };
 
 const type = "aboutus";
 const configName = "general";
 
 export const useCmsAboutUsStore = create((set, get) => ({
-	...initialData,
+  ...initialData,
 
-	setState: (val, prop) => {
-		set({ [prop]: val });
-	},
-	getInitialData: async () => {
-		// set({ rawData: tempAboutUsPayload, data: tempAboutUsPayload });
-		try {
-			let data = await GetConfig(configName, { type: type });
-			data = data.length > 0 ? data[0] : {};
-			set({ rawData: data, data: data });
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	onChangeAttachment: (file, prop) => {
-		const { data } = template(get);
-		if (file.length > 0) {
-			data[prop] = file[0];
-		} else {
-			data[prop] = get().rawData[prop];
-		}
-		set({ data: data });
-	},
-	setDescription: (value) => {
-		const { data, language } = template(get);
-		data[language]["desc"] = value;
-		set({ data: data });
-	},
-	setSmallParagraph: (value) => {
-		const { data, language } = template(get);
-		data[language]["smallparagraph"] = value;
-		set({ data: data });
-	},
-	setVisiMisi: (value, prop, index = null) => {
-		const { data, language } = template(get);
-		const current = data[language]["visimisi"];
-		if (index === null) current[prop] = value;
-		// artinya ini misilist
-		else {
-			current[prop][index] = value;
-		}
-		set({ data: data });
-	},
-	deleteVisiMisi: (index) => {
-		const { data, language } = template(get);
-		data[language]["visimisi"]["misilist"].splice(index, 1);
-		set({ data: data });
-	},
-	addVisiMisi: () => {
-		const { data, language } = template(get);
-		data[language]["visimisi"]["misilist"].push("");
-		set({ data: data });
-	},
-	// setGradeLists: (value, prop, index) => {
-	// 	const { data, language } = template(get);
-	// 	data[language]["gradelists"][index][prop] = value;
-	// 	set({ data: data });
-	// },
-	submitData: async (attachments) => {
-		const tempAtt = {};
-		if (!isObjectEmpty(attachments)) {
-			const promises = await Promise.all(
-				Object.keys(attachments).map((key) => {
-					let formData = new FormData();
-					const theFile = attachments[key];
-					formData.append(key, theFile);
-					return UploadAttachment("assets", formData);
-				})
-			);
-			promises.forEach((res) => {
-				const type = res.data[0].type;
-				tempAtt[type] = res.data[0].fileURL;
-			});
-		}
-		const payload = { ...get().data, ...tempAtt };
-		try {
-			await SubmitConfig(configName, [{ type: type, ...payload }]);
-			set({ loading: false });
-			Swal.fire("Success", "Success to submit data!", "success").then((res) => {
-				if (res.isConfirmed) window.location.reload();
-			});
-		} catch (error) {
-			console.log(error);
-		} finally {
-			set({ loading: false });
-		}
-	},
+  setState: (val, prop) => {
+    set({ [prop]: val });
+  },
+  getInitialData: async () => {
+    // set({ rawData: tempAboutUsPayload, data: tempAboutUsPayload });
+    try {
+      let data = await GetConfig(configName, { type: type });
+      data = data.length > 0 ? data[0] : {};
+      set({ rawData: data, data: data });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  onChangeAttachment: (file, prop) => {
+    const { data } = template(get);
+    if (file.length > 0) {
+      data[prop] = file[0];
+    } else {
+      data[prop] = get().rawData[prop];
+    }
+    set({ data: data });
+  },
+  setDescription: (value) => {
+    const { data, language } = template(get);
+    data[language]["desc"] = value;
+    set({ data: data });
+  },
+  setSmallParagraph: (value) => {
+    const { data, language } = template(get);
+    data[language]["smallparagraph"] = value;
+    set({ data: data });
+  },
+  setMotto: (val) => {
+    const { data, language } = template(get);
+    data[language]["motto"] = val;
+    set({ data: data });
+  },
+  setVisiMisi: (value, prop, index = null) => {
+    const { data, language } = template(get);
+    const current = data[language]["visimisi"];
+    if (index === null) current[prop] = value;
+    // artinya ini misilist
+    else {
+      current[prop][index] = value;
+    }
+    set({ data: data });
+  },
+  deleteVisiMisi: (index) => {
+    const { data, language } = template(get);
+    data[language]["visimisi"]["misilist"].splice(index, 1);
+    set({ data: data });
+  },
+  addVisiMisi: () => {
+    const { data, language } = template(get);
+    data[language]["visimisi"]["misilist"].push("");
+    set({ data: data });
+  },
+  // setGradeLists: (value, prop, index) => {
+  // 	const { data, language } = template(get);
+  // 	data[language]["gradelists"][index][prop] = value;
+  // 	set({ data: data });
+  // },
+  submitData: async (attachments) => {
+    const tempAtt = {};
+    if (!isObjectEmpty(attachments)) {
+      const promises = await Promise.all(
+        Object.keys(attachments).map((key) => {
+          let formData = new FormData();
+          const theFile = attachments[key];
+          formData.append(key, theFile);
+          return UploadAttachment("assets", formData);
+        })
+      );
+      promises.forEach((res) => {
+        const type = res.data[0].type;
+        tempAtt[type] = res.data[0].fileURL;
+      });
+    }
+    const payload = { ...get().data, ...tempAtt };
+    try {
+      await SubmitConfig(configName, [{ type: type, ...payload }]);
+      set({ loading: false });
+      Swal.fire("Success", "Success to submit data!", "success").then((res) => {
+        if (res.isConfirmed) window.location.reload();
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      set({ loading: false });
+    }
+  },
 }));
