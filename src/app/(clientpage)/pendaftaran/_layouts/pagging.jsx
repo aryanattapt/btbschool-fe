d6f64941098b4911fa3aa1c3ffc576ee4e9d367e
Epraldo88 <eko.prasetyo1314@gmail.com>
2025-05-02 19:21:14 +0700

    improve pagging on pendaftaran

--- src/app/(clientpage)/pendaftaran/_layouts/pagging.jsx
+++ src/app/(clientpage)/pendaftaran/_layouts/pagging.jsx
@@ -14,14 +14,20 @@ const PaggingLayouts = ({
     setActiveTab(selectedTab);
   };
 
+  const paginationClick = ["enrolment", "beasiswa", "tur-sekolah"];
+
+  const goToOnlineRegistration = () => {
+    window.location.href = `${pendaftaranData[language]?.pagingHeader?.url[3]?.url}`;
+  };
+
   return (
     <>
       <header className="flex flex-row items-start justify-between text-[#00305E] border-b">
         <div className="inline-flex sm:gap-5 md:gap-11 lg:gap-14 xl:gap-10">
-          <div className="border-r-2">
-            <div className="pr-2 pl-2 md:pr-20 md:pl-32 sm:pr-5 sm:pl-5 md:pr-5 md:pl-5">
+          <div className="border-r-2 flex items-center">
+            <div className="pr-4 pl-4 md:pr-8 md:pl-8 sm:pr-5 sm:pl-5 ">
               <a href="/onlineregistration">
-                <h1 className="text-[25px] md:text-[25px] text-center font-semibold py-8 md:py-10">
+                <h1 className="text-[25px] md:text-[25px] text-center font-semibold">
                   <div className="cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600">
                     {pendaftaranData[language]?.pagingHeader?.title}
                   </div>
@@ -30,8 +36,21 @@ const PaggingLayouts = ({
             </div>
           </div>
           <div className="inline-flex text-[20px] md:text-[20px] text-center text-gray-500 border-gray-200 dark:text-gray-400 dark:border-gray-700">
-            <ul className="flex flex-wrap -mb-px py-8 md:py-10">
-              <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-[100px]">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 -mb-px py-8 md:py-10 px-10 sm:px-4 md:px-2 lg:px-4 xl:px-8 items-center justify-center">
+              {pendaftaranData[language]?.pagingHeader?.url.map((res, idx) => (
+                <LinkItem
+                  title={res?.title}
+                  isActive={activeTab === paginationClick[idx]}
+                  onClick={() =>
+                    idx < 3
+                      ? onClickPagination(paginationClick[idx])
+                      : goToOnlineRegistration()
+                  }
+                />
+              ))}
+            </ul>
+            {/* <ul className="flex flex-wrap -mb-px py-8 md:py-10">
+              <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-8">
                 <div
                   className={`cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600 ${
                     activeTab === "enrolment"
@@ -44,7 +63,7 @@ const PaggingLayouts = ({
                   {pendaftaranData[language]?.pagingHeader?.url[0]?.title}
                 </div>
               </li>
-              <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-[100px]">
+              <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-8">
                 <div
                   className={`cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600 ${
                     activeTab === "beasiswa"
@@ -57,7 +76,7 @@ const PaggingLayouts = ({
                   {pendaftaranData[language]?.pagingHeader?.url[1]?.title}
                 </div>
               </li>
-              <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-[100px]">
+              <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-8">
                 <div
                   className={`cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600 ${
                     activeTab === "tur-sekolah"
@@ -70,7 +89,7 @@ const PaggingLayouts = ({
                   {pendaftaranData[language]?.pagingHeader?.url[2]?.title}
                 </div>
               </li>
-              <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-[100px] text-left">
+              <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-8 text-left">
                 <div
                   className={`cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600 ${
                     activeTab === "onlineregistration"
@@ -84,11 +103,27 @@ const PaggingLayouts = ({
                   {pendaftaranData[language]?.pagingHeader?.url[3]?.title}
                 </div>
               </li>
-            </ul>
+            </ul> */}
           </div>
         </div>
       </header>
     </>
+  );
+};
+
+const LinkItem = ({ isActive, title, onClick }) => {
+  return (
+    <li className="w-full ">
+      {/* <li className="px-10 sm:px-4 md:px-2 lg:px-4 xl:px-8"> */}
+      <div
+        className={`cursor-pointer inline-block border-transparent rounded-t-lg hover:text-blue-600 dark:hover:text-blue-600 ${
+          isActive ? "bg-white text-black underline" : "bg-white"
+        }`}
+        onClick={onClick}
+      >
+        {title}
+      </div>
+    </li>
   );
 };
 
